fd733aa3f972bcc575ef75e91766950d71a32e62
Ivan Babayev <babayev@e2n.de>
2026-06-22 13:37:44 +0200

    Align project structure with E2N CDK workflow

--- src/main/java/de/e2n/cdkhandson/constants/Constants.java
+++ src/main/java/de/e2n/cdkhandson/constants/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 
     public static final String PROJECT_NAME = "cdk-java-serverless-hands-on";
     public static final String WORKSHOP_NAME = "cdk-java";
-    public static final String OWNER = "participant";
+    public static final String OWNER = "developer";
 
     public static final String PRODUCTION_STACK_NAME = "Prod-Serverless-HandsOn";
     public static final String STAGING_STACK_NAME = "Staging-Serverless-HandsOn";
