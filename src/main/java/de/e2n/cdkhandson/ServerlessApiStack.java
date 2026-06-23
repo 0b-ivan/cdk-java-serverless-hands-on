7ded3a04acd84d96879b66c114b1104902e32b55
Ivan Babayev <babayev@e2n.de>
2026-06-23 11:14:27 +0200

    Step 00: Initialize CDK Java project

---
+++ src/main/java/de/e2n/cdkhandson/ServerlessApiStack.java
@@ -0,0 +1,24 @@
+package de.e2n.cdkhandson;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class ServerlessApiStack extends Stack {
+    public ServerlessApiStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public ServerlessApiStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+
+        // example resource
+        // final Queue queue = Queue.Builder.create(this, "MessageQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+    }
+}
